7fa977d583f43c0ab1390b7f29c0c202fcd2386e
ahmedalikeerio <49383934+ahmedalikeerio@users.noreply.github.com>
2020-10-25 01:30:10 +0500

    Create ArithmeticOperators.py

---
+++ Python Operators/ArithmeticOperators.py
@@ -0,0 +1,10 @@
+a = 2
+b = 4
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)         # finds the remainder
+print(a**b)        # Exponentiation - means b is the power of a
+print(a//b)        # Floor division
