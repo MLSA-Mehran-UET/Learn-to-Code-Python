f1eec879dbc3b145b095b53369e3312821b89a4a
Affan Ali <61421532+DevAffan@users.noreply.github.com>
2020-10-25 15:42:28 +0500

    add and sub by using functions

---
+++ Python Basics/basic-Functions.py
@@ -0,0 +1,15 @@
+#using functions to do addition subtraction of two numbers
+#addition function to add two numbers
+def add(num1, num2):
+    return num1 + num2
+
+#subtraction function to subtract two numbers
+def sub(num1, num2):
+    return num1 - num2
+
+
+addition_ans = add(5, 10) #giving two arguments to add them anf store in addition_ans variable
+subtraction_ans = sub(40, 10)#giving two arguments to add them anf store in addition_ans variable
+
+print("Answer after addition:",addition_ans) #printing addition answer
+print("Answer after subtraction:",subtraction_ans) #printing subtraction answer
